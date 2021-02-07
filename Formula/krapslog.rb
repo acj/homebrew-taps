2fc1a64f479f3e076b0189af28faec40b58c6125
Adam Jensen <acjensen@gmail.com>
2021-02-07 12:20:57 -0500

    Add bottle for krapslog 0.1.2 on macOS Big Sur

--- Formula/krapslog.rb
+++ Formula/krapslog.rb
@@ -8,6 +8,7 @@ class Krapslog < Formula
 
   bottle do
     root_url "https://github.com/acj/homebrew-krapslog-rs/releases/download/krapslog-0.1.2"
+    sha256 cellar: :any_skip_relocation, big_sur:      "8d62e37f0598cadc3db27682be3309b88524396f6882d799b2adf038cf9159f6"
     sha256 cellar: :any_skip_relocation, catalina:     "ed31c7462dc09dec3ed70f28614877afdabc289c87eb2743688bf896f79fab4c"
     sha256 cellar: :any_skip_relocation, x86_64_linux: "5bafb2a5e509997666f51ec1df5eb398cf37d0d30f60c9a6f78b537372fecfc4"
   end
