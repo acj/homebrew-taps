329cf498527d6ea11344bd65d8d94befd4fa118f
Adam Jensen <adam@acj.sh>
2023-09-13 07:48:15 -0400

    Upgrade hts to 0.2.0

--- Formula/hts.rb
+++ Formula/hts.rb
@@ -1,8 +1,8 @@
 class Hts < Formula
   desc "Highlight lines of program output based on the latency between them"
   homepage "https://github.com/acj/hts"
-  url "https://github.com/acj/hts/archive/v0.1.0.tar.gz"
-  sha256 "0de6d99a69f7181f1f61cfdccd27e09cbe024fbdbbbba10706aa0b6e63059b4b"
+  url "https://github.com/acj/hts/archive/v0.2.0.tar.gz"
+  sha256 "cc164837d0bbbb6925351116d5118d85598a9411a636e2a54ea74db212096e97"
   license "MIT"
   head "https://github.com/acj/hts.git"
 
