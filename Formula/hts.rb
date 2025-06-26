2e5e618d00fc1dd250de3153915cb7b64a40bc9a
Adam Jensen <adam@acj.sh>
2025-06-25 21:59:22 -0400

    Fix commit for hts v0.2.0

--- Formula/hts.rb
+++ Formula/hts.rb
@@ -2,7 +2,7 @@ class Hts < Formula
   desc "Highlight lines of program output based on the latency between them"
   homepage "https://github.com/acj/hts"
   url "https://github.com/acj/hts/archive/v0.2.0.tar.gz"
-  sha256 "cc164837d0bbbb6925351116d5118d85598a9411a636e2a54ea74db212096e97"
+  sha256 "bd083470b14b6f0cd288cf9354756d20c78ff1e1f94a6b693e86ac89eac7f484"
   license "MIT"
   head "https://github.com/acj/hts.git"
 
